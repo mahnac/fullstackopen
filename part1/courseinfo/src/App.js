c8cd31cb9bf3af495c8381e8aee39ee2556993ad
James Sejer Canham <jamessejercanham@gmail.com>
2021-10-11 19:02:51 +0200

    exercise 1.4 course information done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,26 +2,20 @@ import React from 'react'
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {name: 'Fundamentals of React', exercises: 10}
-    const part2 = {name: 'Using props to pass data', exercises: 7}
-    const part3 = {name: 'State of a component', exercises: 14}
+    const parts = [
+        {name: 'Fundamentals of React', exercises: 10},
+        {name: 'Using props to pass data', exercises: 7},
+        {name: 'State of a component', exercises: 14}
+    ]
 
     return (
         <div>
             <Header course={course}/>
-            <Content
-                part1={part1}
-                part2={part2}
-                part3={part3}
-            />
-            <Total
-                part1={part1}
-                part2={part2}
-                part3={part3}
-            />
+            <Content parts={parts}/>
+            <Total parts={parts}/>
 
         </div>
-    )
+    );
 }
 
 const Header = ({course}) => {
@@ -32,9 +26,7 @@ const Content = (props) => {
     console.log(props)
     return (
         <>
-            <Part name={props.part1.name} exercises={props.part1.exercises} />
-            <Part name={props.part2.name} exercises={props.part2.exercises} />
-            <Part name={props.part3.name} exercises={props.part3.exercises} />
+            {props.parts.map((part, index) => <Part key={index} name={part.name} exercises={part.exercises}/> )}
         </>
     )
 }
@@ -50,9 +42,10 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+    console.log(props)
     return (
         <>
-            <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+            <p>Number of exercises {props.parts.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)}</p>
         </>
     )
 }
