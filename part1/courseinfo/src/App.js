297eeee3064b4b3f483775722c936372e56d9013
James Sejer Canham <jamessejercanham@gmail.com>
2021-10-11 19:07:20 +0200

    exercise 1.5 course information done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,51 +1,53 @@
 import React from 'react'
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {name: 'Fundamentals of React', exercises: 10},
-        {name: 'Using props to pass data', exercises: 7},
-        {name: 'State of a component', exercises: 14}
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {name: 'Fundamentals of React', exercises: 10},
+            {name: 'Using props to pass data', exercises: 7},
+            {name: 'State of a component', exercises: 14}
+        ]
+    }
 
     return (
         <div>
             <Header course={course}/>
-            <Content parts={parts}/>
-            <Total parts={parts}/>
-
+            <Content course={course}/>
+            <Total course={course}/>
         </div>
     );
 }
 
 const Header = ({course}) => {
-    return <h1>{course}</h1>
+    return <h1>{course.name}</h1>
 }
 
-const Content = (props) => {
-    console.log(props)
+const Content = ({course}) => {
+    console.log(course)
     return (
         <>
-            {props.parts.map((part, index) => <Part key={index} name={part.name} exercises={part.exercises}/> )}
+            {course.parts.map((part, index) => <Part key={index} name={part.name} exercises={part.exercises}/>)}
         </>
     )
 }
 
-const Part = (props) => {
+const Part = ({name, exercises}) => {
     return (
         <>
             <p>
-                {props.name} {props.exercises}
+                {name} {exercises}
             </p>
         </>
     )
 }
 
-const Total = (props) => {
-    console.log(props)
+const Total = ({course}) => {
+    console.log(course)
     return (
         <>
-            <p>Number of exercises {props.parts.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)}</p>
+            <p>Number of
+                exercises {course.parts.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)}</p>
         </>
     )
 }
